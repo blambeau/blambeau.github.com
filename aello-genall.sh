14b152f94516bc26cc7e0253d979f5f63374eb1c
Bernard Lambeau <blambeau@gmail.com>
2009-01-14 14:31:19 +0000

    Fixes a bug in aello-genall
    git-svn-id: http://svn.chefbe.net/org.revision-zero@13 ce6f69d7-1511-4e9e-a9c4-3d9749c48749

--- aello-genall.sh
+++ aello-genall.sh
@@ -1,7 +1,10 @@
-export CODE=/home/blambeau/revision-zero/code/ruby/web_duck_typing
-export SOURCE=/home/blambeau/revision-zero/articles
-export TARGET=/home/revision-zero/public_html/statics
-export BASE=http://www.revision-zero.org/
+CODE=/home/blambeau/revision-zero/code/ruby/web_duck_typing
+SOURCE=/home/blambeau/revision-zero/articles
+SRC_PUBLIC=/home/blambeau/revision-zero/public
+TARGET=/home/revision-zero/public_html/statics
+TRG_PUBLIC=/home/revision-zero/public_html
+BASE=http://www.revision-zero.org/
 
-$CODE/genrevzero.rb --verbose --single $SOURCE/404.r0 $TARGET/404.html --template $SOURCE/404.wtpl -Sbase=$BASE
+cp $SRC_PUBLIC/.htaccess $TRG_PUBLIC
+$CODE/genrevzero.rb --verbose --from $SOURCE --to $TARGET --single $SOURCE/404.r0 $TARGET/404.html --template $SOURCE/404.wtpl -Sbase=$BASE
 $CODE/genrevzero.rb --verbose --from $SOURCE --to $TARGET --template $SOURCE/template.wtpl -Sbase=$BASE
