ebab33bdf91511e2f36a1c323e4ea558fb6b1180
Bernard Lambeau <blambeau@gmail.com>
2009-01-14 15:05:56 +0000

    Updates aello-genall.
    git-svn-id: http://svn.chefbe.net/org.revision-zero@16 ce6f69d7-1511-4e9e-a9c4-3d9749c48749

--- aello-genall.sh
+++ aello-genall.sh
@@ -6,5 +6,11 @@ TRG_PUBLIC=/home/revision-zero/public_html
 BASE=http://www.revision-zero.org/
 
 cp $SRC_PUBLIC/.htaccess $TRG_PUBLIC
+rm $TARGET/* -rf
+cp $SRC_PUBLIC/statics/* $TARGET -R
+rm $TARGET/.svn -rf
+rm $TARGET/images/.svn -rf
 $CODE/genrevzero.rb --verbose --from $SOURCE --to $TARGET --single $SOURCE/404.r0 $TARGET/404.html --template $SOURCE/404.wtpl -Sbase=$BASE
 $CODE/genrevzero.rb --verbose --from $SOURCE --to $TARGET --template $SOURCE/template.wtpl -Sbase=$BASE
+chmod ug+r,o+r,og-w $TARGET -R
+chmod ugo+x $TARGET/images
