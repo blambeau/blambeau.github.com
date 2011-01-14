40b52168336b6e5d2d6b79ab7ba326f0cd9752f8
Bernard Lambeau <blambeau@gmail.com>
2011-01-14 20:08:48 +0100

    Added an article about Noe

---
+++ src/articles/noe/starter.sh
@@ -0,0 +1,15 @@
+blambeau@yemana % noe prepare --template=ruby hello_world
+blambeau@yemana % cd hello_world 
+blambeau@yemana % noe go     
+blambeau@yemana % ls -lA
+-rw-r--r--  1 blambeau  staff    49 Jan 14 17:21 .gitignore
+-rw-r--r--  1 blambeau  staff    48 Jan 14 17:21 CHANGELOG.md
+-rw-r--r--  1 blambeau  staff    59 Jan 14 17:21 Gemfile
+-rw-r--r--  1 blambeau  staff  1133 Jan 14 17:21 LICENCE.md
+-rw-r--r--  1 blambeau  staff    96 Jan 14 17:21 README.md
+-rw-r--r--  1 blambeau  staff   542 Jan 14 17:21 Rakefile
+-rw-r--r--  1 blambeau  staff  5902 Jan 14 17:21 hello_world.gemspec
+-rw-r--r--  1 blambeau  staff  6860 Jan 14 17:21 hello_world.noespec
+drwxr-xr-x  4 blambeau  staff   136 Jan 14 17:21 lib
+drwxr-xr-x  4 blambeau  staff   136 Jan 14 17:21 spec
+drwxr-xr-x  6 blambeau  staff   204 Jan 14 17:21 tasks
